ce7dd0e6ad991748e1c5017a307b679ca6f61340
manvi singh <singh11manvi@gmail.com>
2019-05-05 12:17:33 +0000

    created the UI

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,6 +6,20 @@ sap.ui.define([
 	return Controller.extend("com.sap.booking.controller.View1", {
 		onInit: function () {
 
+		},
+
+		onValueHelpRequest: function() {
+			var that = this;
+			this.TypeDialog = new sap.m.SelectDialog({
+				title: "Site Change",
+				growingThreshold: 1000,
+				growingScrollToLoad: true,
+				growing: true,
+				noDataText: "No Entries Found",
+
+				liveChange: [that.typeSearch, that],
+				confirm: [that.typeConfrm, that]
+			});
 		}
 	});
 });
