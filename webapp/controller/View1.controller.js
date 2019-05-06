41bc8e25abbafef7626a852dac397e53c8e89beb
manvi singh <singh11manvi@gmail.com>
2019-05-06 16:34:50 +0000

    Created metadata and mock data  for filters fields Bound the filters fields to mockserver data

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter"
+], function (Controller, Filter) {
 	"use strict";
 
 	return Controller.extend("com.sap.booking.controller.View1", {
@@ -8,18 +9,46 @@ sap.ui.define([
 
 		},
 
-		onValueHelpRequest: function() {
-			var that = this;
-			this.TypeDialog = new sap.m.SelectDialog({
-				title: "Site Change",
-				growingThreshold: 1000,
-				growingScrollToLoad: true,
-				growing: true,
-				noDataText: "No Entries Found",
+		handleValueHelp: function (oEvent) {
+			var sInputValue = oEvent.getSource().getValue();
 
-				liveChange: [that.typeSearch, that],
-				confirm: [that.typeConfrm, that]
-			});
+			this.inputId = oEvent.getSource().getId();
+			// create value help dialog
+			if (!this._valueHelpDialog) {
+				this._valueHelpDialog = sap.ui.xmlfragment(
+					"com.sap.booking.view.Approve",
+					this
+				);
+				this.getView().addDependent(this._valueHelpDialog);
+			}
+
+			// create a filter for the binding
+			this._valueHelpDialog.getBinding("items").filter([new Filter(
+				"ProjectName",
+				sap.ui.model.FilterOperator.Contains, sInputValue
+			)]);
+
+			// open value help dialog filtered by the input value
+			this._valueHelpDialog.open(sInputValue);
+		},
+
+		_handleValueHelpSearch: function (evt) {
+			var sValue = evt.getParameter("value");
+			var oFilter = new Filter(
+				"ProjectName",
+				sap.ui.model.FilterOperator.Contains, sValue
+			);
+			evt.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		_handleValueHelpClose: function (evt) {
+			var oSelectedItem = evt.getParameter("selectedItem");
+			if (oSelectedItem) {
+				var productInput = this.getView().byId(this.inputId);
+				productInput.setValue(oSelectedItem.getTitle());
+			}
+			evt.getSource().getBinding("items").filter([]);
 		}
+
 	});
 });
